3023fe65afbe53ddf0998756f49d379e56a2f6c4
FredrikGordh <fredrik.gordh@gmail.com>
2024-07-23 15:11:48 +0200

    minor change to settings.py

--- django_server_app/settings.py
+++ django_server_app/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from environs import Env
 env = Env()
 env.read_env()
+print(env.str("SECRET_KEY"))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
