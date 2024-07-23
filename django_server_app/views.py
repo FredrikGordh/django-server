546f88744c700e1f0d16ee47ec8b52bc49851e8a
FredrikGordh <fredrik.gordh@gmail.com>
2024-07-23 12:13:43 +0200

    testing to change a view

--- django_server_app/views.py
+++ django_server_app/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 
 def hello_world(request):
-    return HttpResponse("Hello World!")
+    return HttpResponse("Hello World 2!")
 
 def home(request):
     return HttpResponse("Welcome to the Homepage!")
