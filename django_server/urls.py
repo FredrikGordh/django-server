f1bd6d38e3770c77401bd1178185d97668bc28b7
FredrikGordh <fredrik.gordh@gmail.com>
2024-07-23 15:38:14 +0200

    small changes

--- django_server/urls.py
+++ django_server/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django_server.views import hello_world, home, data_view
+from django_server.views import hello_world, home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', hello_world),
     path('', home),
-    path('api/data/', data_view, name='data_view'),
+    # path('api/data/', data_view, name='data_view'),
 
 ]
--- django_server/views.py
+++ django_server/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse, JsonResponse
-from .models import YourModel
+# from .models import YourModel
+
 def hello_world(request):
     return HttpResponse("Hello World 2!")
 
 def home(request):
     return HttpResponse("Welcome to the Homepage!")
 
-def data_view(request):
-    data = list(YourModel.objects.values())
-    return JsonResponse(data, safe=False)
+# def data_view(request):
+#     data = list(YourModel.objects.values())
+#     return JsonResponse(data, safe=False)
